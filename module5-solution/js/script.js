664e67df8b96ce78463fabd588b555c42142d30b
Ian <insllvn@gmail.com>
2021-01-04 12:06:30 -0500

    testing 1

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -14,12 +14,10 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 var dc = {};
 
 var homeHtmlUrl = "snippets/home-snippet.html";
-var allCategoriesUrl =
-  "https://davids-restaurant.herokuapp.com/categories.json";
+var allCategoriesUrl = "https://davids-restaurant.herokuapp.com/categories.json";
 var categoriesTitleHtml = "snippets/categories-title-snippet.html";
 var categoryHtml = "snippets/category-snippet.html";
-var menuItemsUrl =
-  "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
+var menuItemsUrl = "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
 var menuItemsTitleHtml = "snippets/menu-items-title.html";
 var menuItemHtml = "snippets/menu-item.html";
 
@@ -99,7 +97,7 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
         
-      var chosenCategoryShortName = chooseRandomCategory(categories);
+      var chosenCategoryShortName = chooseRandomCategory('categories');
       
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -113,12 +111,13 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       
-      var homeHtmlToInsertIntoMainPage = insertProperty(html, "chosenCategoryShortName", name);
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "'" + randomCategoryShortName + "'", "'" + chosenCategoryShortName + "'");
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
-      // ....
+      
+      var randomViewHtml = insertHtml("#main-content", "'" + randomCategoryShortName + "'");
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
